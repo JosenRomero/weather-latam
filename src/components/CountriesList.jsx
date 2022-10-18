3de88a74d856c0d8864021fdedf29bad43494d9e
Josen Romero <romerojosemanuel@hotmail.com>
2022-10-17 18:38:00 -0600

    add react-responsive-carousel

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -2,6 +2,8 @@ import { useWeather } from '../hooks/useWeather'
 import { countriesInfo } from '../countriesInfo'
 import CountryCard from './CountryCard'
 import lightning_and_rainy from '../images/lightning_and_rainy.svg'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import { Carousel } from 'react-responsive-carousel'
 
 const CountriesList = () => {
 
@@ -10,20 +12,32 @@ const CountriesList = () => {
   if(!data) return <></>
 
   return (
-    <div className="md:w-3/5 mx-auto">
+    <div className="md:w-3/4 mx-auto">
       <div className="p-6 m-2 bg-white rounded-xl text-zinc-800">
 
-      {data.map((obj, i) => {
-        return <CountryCard
-          key={i}
-          img={countriesInfo[obj.sys.country].img}
-          country={countriesInfo[obj.sys.country].country}
-          city={countriesInfo[obj.sys.country].city}
-          temp_max={obj.main.temp_max}
-          temp_min={obj.main.temp_min}
-          icon={lightning_and_rainy}
-        />
-      })}
+        <Carousel
+          autoFocus={false}
+          autoPlay={false}
+          emulateTouch={false}
+          infiniteLoop={false}
+          showStatus={false}
+          showIndicators={false}
+          showThumbs={false}
+        >
+
+          {data.map((obj, i) => {
+            return <CountryCard
+              key={i}
+              img={countriesInfo[obj.sys.country].img}
+              country={countriesInfo[obj.sys.country].country}
+              city={countriesInfo[obj.sys.country].city}
+              temp_max={obj.main.temp_max}
+              temp_min={obj.main.temp_min}
+              icon={lightning_and_rainy}
+            />
+          })}
+        
+        </Carousel>
 
       </div>
     </div>
--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -2,14 +2,14 @@
 const CountryCard = ({img, country, city, temp_max, temp_min, icon}) => {
 
   return (
-    <div className="flex items-center text-3xl">
-      <img src={img} className="w-32 flex-none" alt={country} />
-      <div className="ml-4 grow">
+    <div className="flex items-center text-xl md:text-3xl my-8">
+      <img src={img} className="!w-32 md:!w-40 flex-none" alt={country} />
+      <div className="ml-4 grow text-start">
         <p className="pb-3">{country}</p>
         <p>{city}</p>
       </div>
-      <img src={icon} className="w-32 flex-none" alt={country} />
-      <div className="ml-4 flex-none">
+      <img src={icon} className="!w-16 md:!w-32 flex-none" alt={country} />
+      <div className="ml-6 flex-none">
         <p className="bg-red-200 p-2">{Math.floor(temp_max)}°</p>
         <p className="bg-blue-200 p-2">{Math.floor(temp_min)}°</p>
       </div>
