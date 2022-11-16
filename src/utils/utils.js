54da99b1e3348ba627369a374f735e04ea43a55e
Josen Romero <romerojosemanuel@hotmail.com>
2022-11-15 18:50:40 -0600

    update utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,6 +2,7 @@
 export const formatDate = (date, timezone) => {
   let myDate = new Date((timezone + date) * 1000)
   let hours = myDate.getUTCHours()
-  let minutes = (myDate.getUTCSeconds() > 30) ? myDate.getUTCMinutes() + 1 : myDate.getUTCMinutes()
+  let minutes = (myDate.getUTCSeconds() >= 30) ? myDate.getUTCMinutes() + 1 : myDate.getUTCMinutes()
+  minutes = (minutes < 10) ? `0${minutes}` : minutes
   return `${hours}:${minutes}`
 }
